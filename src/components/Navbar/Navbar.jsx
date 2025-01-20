75ceb2ac17fd2d624b97114b1d2ee1eb44623f24
Avi Israeli <aviv8710@gmail.com>
2025-01-20 11:56:26 +0200

    logout functionality applied

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/aiflogo4@600x.png";
-
 import "./Navbar.css";
-import firebase from "firebase/compat/app";
+import { getAuth, signOut } from "firebase/auth";
 
 const Navbar = () => {
+  const navigate = useNavigate(); // Use the useNavigate hook to get the navigate function
+
   const handleLogoutBtn = () => {
-    firebase.auth().signOut();
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log("User signed out");
+      navigate("/"); // Redirect to the login page
+    }).catch((error) => {
+      console.error("Error signing out: ", error);
+    });
   };
 
   return (
@@ -14,10 +21,9 @@ const Navbar = () => {
       <ul>
         <div className="logo-container">
           <NavLink to="/dashboardPage">
-            <img src={logo} alt="" />
+            <img src={logo} alt="Logo" />
           </NavLink>
         </div>
-
         <li>
           <NavLink to="/traineesPage">Trainees</NavLink>
         </li>
@@ -29,7 +35,7 @@ const Navbar = () => {
             onClick={handleLogoutBtn}
             className="secondary-btn logout-btn"
           >
-            Log Out{" "}
+            Log Out
           </button>
         </li>
       </ul>
